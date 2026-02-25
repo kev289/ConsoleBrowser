2618d8a2e099c37d2fbdb44e53e85eaf83bb190c
KevinU <kevinuribe114@gmail.com>
2026-02-25 10:24:02 -0500

    updateenv

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 const pool = mysql.createPool({
-	host: process.env.DB_HOST || 'localhost',
-	user: process.env.DB_USER || 'root',
-	password: process.env.DB_PASS || 'Asd.123*',
-	database: process.env.DB_NAME || 'Oficina',
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME,
 	waitForConnections: true,
 	connectionLimit: 10,
 	queueLimit: 0
